f4e410dbda7770c0db23b4c6a3bf4bf12d039d64
David Muriithi <88942514+BarriosXJavier@users.noreply.github.com>
2024-11-27 10:32:31 +0000

    fix auth bugs

--- matrix_blog/app/api/auth/[...nextauth]/route.ts
+++ matrix_blog/app/api/auth/[...nextauth]/route.ts
@@ -1,66 +1,21 @@
-import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-import bcrypt from "bcryptjs";
+import GithubProvider from "next-auth/providers/github";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+const authOptions = {
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null;
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-        if (user && bcrypt.compareSync(credentials.password, user.password)) {
-          return user;
-        }
-        return null;
-      },
-    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-  callbacks: {
-    session: async ({ session, token }) => {
-      session.user = {
-        ...session.user,
-        id: token.id,
-      };
-      return session;
-    },
-    jwt: ({ token, user }) => {
-      if (user) {
-        const u = user as unknown as any;
-        return {
-          ...token,
-          id: u.id,
-        };
-      }
-      return token;
-    },
-  },
-  debug: true,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
